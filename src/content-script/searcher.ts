e5e743002c2fac75421a6ecc4d15b616b7910c5d
Justice Ogbonna <justiceo@users.noreply.github.com>
2023-12-04 00:34:19 -0800

    Integrate flexsearch

--- src/content-script/searcher.ts
+++ src/content-script/searcher.ts
@@ -1,10 +1,42 @@
+import Document from "flexsearch/src/document";
 import emojiList from "../standalone/emoji-list.json";
+import { Logger } from "../utils/logger";
 
 export class Searcher {
+  doc = new Document({
+    preset: "score",
+    tokenize: "full",
+    charset: "latin:advanced",
+    cache: true,
+    id: "emoji",
+    index: ["alternates"],
+  });
+  logger = new Logger(this);
+
+  constructor() {
+    emojiList.forEach((emoji) => {
+      this.doc.add(emoji.emoji, emoji);
+    });
+  }
+
   search(query: string) {
-    const results = emojiList.filter((emoji) =>
-      emoji.alternatesStr.includes(query)
+    this.logger.debug("Searching for:", query);
+
+    const resultIndices = this.doc.search(query, {
+      limit: 100,
+      suggest: true,
+      index: ["alternates"],
+      bool: "or",
+    });
+    if (resultIndices.length === 0) {
+      return [];
+    }
+
+    const matchingEmojis = resultIndices[0].result.map((r) =>
+      emojiList.find((e) => e.emoji === r)
     );
-    return results;
+
+    this.logger.debug("Search results:", matchingEmojis);
+    return matchingEmojis;
   }
 }
